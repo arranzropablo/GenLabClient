fd5ae7cab48b8e128e3eb433e4db40d9f66701c2
CharlieGnomo <carlomaroficial@gmail.com>
2018-02-12 21:31:52 +0100

    Test view horizontal scroll fixed

--- genlab/www/resources/js/home.js
+++ genlab/www/resources/js/home.js
@@ -21,8 +21,8 @@ $(document).ready(function() {
 
     $("#app_sections").on("click", "li", (event) => {
         $("#homeView").hide();
+        $("#testsView").hide();
         $("#sectionView").show();
-        $("#sectionContent").html("");
         let title;
         let section = $(event.target).parent();
         if (section.data("section") === "Theory") {
@@ -32,38 +32,10 @@ $(document).ready(function() {
         } else if (section.data("section") === "CTools") {
             title = "Calculation Tools";
         } else if (section.data("section") === "Tests") {
-
             title = "Tests";
-            //Obtener número de preguntas
+            //Obtener numero de preguntas
             let num = 10;
-            let testContent = `<div class="row">
-                        <div id="question_1" class="questionCont col-sm-6 col-sm-offset-3">
-                            <div class="form-top">
-                                <div class="form-top-left">
-                                    <h3>1. Question Title</h3>
-                                </div>
-                                <div class="form-top-right">
-                                    <h4>1/`+num+`</h3>
-                                </div>
-                            </div>
-                            <div class="form-bottom">
-                                <div class="login-form">
-                                    <div class="form-group answerCont" id="answer_1_1" data-selected="False">1) Answer 1.1</div>
-                                    <div class="form-group answerCont" id="answer_1_2" data-selected="False">2) Answer 1.2</div>
-                                    <div class="form-group answerCont" id="answer_1_3" data-selected="False">3) Answer 1.3</div>
-                                    <div id="dirButtons" class="form-group">
-                                        <button id="btnNext" class="btnDir">&#60;&#60;</button>
-                                        <button id="btnNext" class="btnDir">&#62;&#62;</button>
-                                    </div>
-                                    <div id="login-buttons" class="form-group form-buttons">
-                                        <button id="btnSaveTest" class="btn btn-success">Save</button>
-                                        <button id="btnRstTest" class="btn btn-info">Reset</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>`;
-            $("#sectionContent").html(testContent);
+            $("#testsView").show();
             $(".answerCont").on("click", (event) => {
                 let answer = $(event.target);
                 let sel = $(answer).data("selected");
